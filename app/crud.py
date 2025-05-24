ada8e9e8c0f4921775eaabf45e504b5bc368d585
spirteque <xyoh.sb@gmail.com>
2025-05-24 18:06:23 +0200

    Create basic functions for POST, CREATE, DELETE actions.

---
+++ app/crud.py
@@ -0,0 +1,23 @@
+from sqlmodel import Session, select
+
+from models import Item
+
+
+def create_item(session: Session, item: Item) -> Item:
+	session.add(item)
+	session.commit()
+	session.refresh(item)
+	return item
+
+
+def get_items(session: Session):
+	return session.exec(select(Item)).all()
+
+
+def delete_item(session: Session, item_id: int) -> bool:
+	item = session.get(Item, item_id)
+	if item:
+		session.delete(item)
+		session.commit()
+		return True
+	return False
